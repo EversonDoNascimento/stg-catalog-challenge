40161dec457c5a60bb85e21fc00be5bcbd39ebb5
EversonDoNascimento <everson25nascimento@gmail.com>
2025-08-06 20:12:54 -0300

    feat: Adicionando paleta de cores no arquivo de configurações do tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,17 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#00695C",
+        secondary: "#0277BD",
+        accent: "#26A69A",
+        info: "#29B6F6",
+        success: "#2E7D32",
+        warning: "#F9A825",
+        danger: "#C62828",
       },
+    },
+    screens: {
+      md: "850px",
     },
   },
   plugins: [],
