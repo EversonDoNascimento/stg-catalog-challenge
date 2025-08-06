098fb5f1bc002e0dfdcb3adb258537105e30163d
EversonDoNascimento <everson25nascimento@gmail.com>
2025-08-06 20:18:36 -0300

    feat: Criando contexts para Autenticação e Carrinho de compras

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { createContext, useContext, useEffect, useState } from "react";
+import { Session, User } from "@supabase/supabase-js";
+import { supabase } from "../libs/supabase/supabase";
+
+interface AuthContextType {
+  user: User | null;
+  session: Session | null;
+  loading: boolean;
+  signOut: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [session, setSession] = useState<Session | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const currentSession = supabase.auth.getSession().then(({ data }) => {
+      setSession(data.session);
+      setUser(data.session?.user ?? null);
+      setLoading(false);
+    });
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+        setUser(session?.user ?? null);
+      }
+    );
+
+    return () => {
+      listener?.subscription.unsubscribe();
+    };
+  }, []);
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+    setSession(null);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, session, loading, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
+};
---
+++ contexts/CartContext.tsx
@@ -0,0 +1,109 @@
+"use client";
+import { supabase } from "@/libs/supabase/supabase";
+import { ProductType } from "@/types/product";
+import { User } from "@supabase/supabase-js";
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from "react";
+import { useAuth } from "./AuthContext";
+
+type CartItem = {
+  id: string;
+  product_id: string;
+  user_id: string;
+  quantity: number;
+  // Você pode adicionar mais dados do produto aqui se quiser
+};
+
+type CartContextType = {
+  items: CartItem[];
+  addToCart: (
+    product: ProductType,
+    user: User
+  ) => Promise<{ data: any; error: any }>;
+  removeFromCart: (productId: string) => Promise<{ error: any }>;
+  //   updateQuantity: (productId: string, quantity: number) => void
+  //   clearCart: () => void
+};
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const user = useAuth()?.user;
+  const [items, setItems] = useState<CartItem[]>([]);
+
+  const getCartItems = async (): Promise<{ data: any; error: any }> => {
+    if (!user) return { data: [], error: null };
+    const { data, error } = await supabase
+      .from("cart_items")
+      .select("*")
+      .eq("user_id", user?.id);
+    return { data, error };
+  };
+  const addToCart = async (product: ProductType, user: User) => {
+    const { data, error } = await supabase
+      .from("cart_items")
+      .insert({ user_id: user.id, product_id: product.id, quantity: 1 });
+    if (!error) {
+      const { data } = await getCartItems();
+      if (data) {
+        setItems(data);
+      }
+    }
+    return { data, error };
+  };
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getCartItems();
+      if (error) return;
+      setItems(data);
+    })();
+  }, [user]);
+  const removeFromCart = async (productId: string) => {
+    const { error } = await supabase
+      .from("cart_items")
+      .delete()
+      .eq("product_id", productId);
+    if (!error) {
+      const { data } = await getCartItems();
+      if (data) {
+        setItems(data);
+      }
+    }
+    return { error };
+  };
+
+  //   const updateQuantity = (productId: string, quantity: number) => {
+  //     if (quantity <= 0) {
+  //       removeFromCart(productId)
+  //     } else {
+  //       setItems((prev) =>
+  //         prev.map((item) =>
+  //           item.productId === productId ? { ...item, quantity } : item
+  //         )
+  //       )
+  //     }
+  //   }
+
+  //   const clearCart = () => {
+  //     setItems([]);
+  //   };
+
+  return (
+    <CartContext.Provider value={{ items, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+};
